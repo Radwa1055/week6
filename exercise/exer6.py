d3b19f8b3e4d5243dbd47d6fd857bfed5d8ffb3c
Radwa1055 <radwa.r2020@gmail.com>
2025-10-04 22:30:59 +0200

    add exer6

---
+++ exercise/exer6.py
@@ -0,0 +1,9 @@
+#Exercise 1: Understanding the Importance of Testing
+#Instruction:
+#Write a small Python function (e.g., a function to calculate the square of a number) and intentionally introduce a bug (e.g., incorrect calculation log0ic).
+
+
+def square(number):
+    return number + number
+print(square(5))
+
